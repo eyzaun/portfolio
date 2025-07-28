950da6f6eb7ffad01df02d7dbf58c9e2121a8bf9
Eyüp Zafer Ünal <eyupzaferunal@gmail.com>
2025-07-28 20:38:28 +0300

    Update config for eyzaun2.github.io deployment

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: './',
-  base: '/eyzaun.github.io2/', // GitHub Pages için repository base path
+  base: '/', // .github.io repository'si için root path
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
